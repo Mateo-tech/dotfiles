8cf4da15e73ea35bba7aba127d553a7b6f85c2bd
Matěj Hladký <67378935+Mateo-tech@users.noreply.github.com>
2022-07-04 09:45:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+## ARCH INSTALLATION ON NON-UEFI SYSTEMS ##
+# fdisk -l
+# fdisk /dev/sda
+# d
+# n <Enters>
+# w
+# mkfs.ext4 /dev/sda1
+# iwctl
+# device list
+# station wlan0 scan
+# station wlan0 get-networks
+# station wlan0 connect <Name>
+# pacman -Syy
+# pacman -S reflector
+# reflector  -c "CZ" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+# mount /dev/sda1 /mnt
+# pacstrap /mnt base linux linux-firmware vim
+# genfstab -U /mnt >> /mnt/etc/fstab
+# arch-chroot /mnt
+# timedatectl set-timezone Europe/Prague
+# vim /etc/locale.gen ==> Enable
+# locale-gen
+# echo LANG=en_GB.UTF-8 > /etc/locale.conf
+# export LANG=en_GB.UTF-8
+# echo arch > /etc/hostname
+# vim /etc/hosts
+# 	127.0.0.1	localhost
+# 	::1		localhost
+# 	127.0.1.1	arch 
+# passwd ==> root
+# pacman -S grub
+# grub-install /dev/sda
+# grub-mkconfig -o /boot/grub/grub.cfg
+# pacman -S sudo
+# useradd -m <Username>
+# passwd <Username>
+# usermod -aG wheel,audio,video,storage <Username>
+# EDITOR=vim visudo ==> Uncomment %wheel ALL(ALL:ALL) ALL
+# pacman -S xorg networkmanager
+# systemctl enable NetworkManager.service
+
 # Check ping before doing anything else
 echo "Checking internet connection..."
 while ! ping -c1 google.com &>/dev/null
